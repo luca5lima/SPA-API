9f28a255a8185ff095761e03f328bb02cbaeb085
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-05-23 21:55:24 -0300

    Adicionar arquivos de projeto.

---
+++ SPA.Api/Data/MyDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SPA.Api.Models;
+
+namespace SPA.Api.Data
+{
+    public class MyDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "SPAdb");
+        }
+
+        public DbSet<Despesa> Despesas { get; set; }
+    }
+}
---
+++ SPA.Api/Models/Despesa.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace SPA.Api.Models
+{
+    public class Despesa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public float Value { get; set; }
+
+        public Despesa()
+        {
+            Description = string.Empty;
+        }
+    }
+}
